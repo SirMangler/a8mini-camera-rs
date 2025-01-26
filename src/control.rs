840768794b4f7f0b6073badfe8e3752cf39debdf
JMST <jiamingsitu06@gmail.com>
2025-01-25 19:36:35 -0500

    Project renaming and restructuring

---
+++ src/control.rs
@@ -0,0 +1,16 @@
+
+pub enum A8MiniCommand {
+	
+}
+
+pub enum A8MiniResponse {
+
+}
+
+pub enum A8MiniMode {
+
+}
+
+pub enum A8MiniSetting {
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
 #![allow(non_snake_case)]
 
+pub mod checksum;
 pub mod constants;
 pub mod control;
-pub mod udp;
+
+pub struct A8Mini {}
+
+impl A8Mini {
+	pub fn connect() -> Result<(), std::io::Error> {
+		Ok(())
+	}
+}
+
+
 
 #[cfg(test)]
-mod tests {
-  // use super::*;
-
-
-}
+mod tests {}
