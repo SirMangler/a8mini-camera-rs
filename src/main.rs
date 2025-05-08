b0c81619cd24e02b9184571d8f184502aad110f9
JMST <jiamingsitu06@gmail.com>
2025-05-08 14:52:41 -0400

    Redo main and update with new commands

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,58 @@
 use std::error::Error;
 use std::io;
 
-use a8mini_camera_rs::control::{A8MiniSimpleCommand, A8MiniComplexCommand};
+use a8mini_camera_rs::control::{A8MiniComplexCommand, A8MiniSimpleCommand, A8MiniSimpleHTTPQuery, A8MiniComplexHTTPQuery};
 use a8mini_camera_rs::A8Mini;
 
 
 fn print_ascii_command_table() {
-  let commands = [
+  let simple_commands = [
     "AutoCenter", "RotateUp", "RotateDown", "RotateRight", "RotateLeft", "StopRotation", "ZoomIn", "ZoomOut", "ZoomMax",
     "MaxZoomInformation", "FocusIn", "FocusOut", "TakePicture", "RecordVideo", "Rotate100100", "CameraInformation",
     "AutoFocus", "HardwareIDInformation", "FirmwareVersionInformation", "SetLockMode", "SetFollowMode", "SetFPVMode",
     "AttitudeInformation", "SetVideoOutputHDMI", "SetVideoOutputCVBS", "SetVideoOutputOff", "LaserRangefinderInformation", 
-    "RebootCamera", "RebootGimbal", "Resolution4k", "Heartbeat", "SetYawPitchSpeed(yaw, pitch)", "SetYawPitchAngle(yaw, pitch)"
+    "RebootCamera", "RebootGimbal", "Resolution4k", "Heartbeat"
   ];
 
-  let header = "+----+------------------------------+";
-  println!("{}", header);
-  println!("| ID | Command Name                 |");
-  println!("{}", header);
+  let complex_commands = [
+    "SetYawPitchSpeed(i8, i8)",
+    "SetYawPitchAngle(i16, i16)",
+    "SetTimeUTC(u64)",
+    "GetCodecSpecs(u8)",
+    "SetCodecSpecs(u8, u8, u16, u16, u16, u8)",
+  ];
 
-  for (i, command) in commands.iter().enumerate() {
-    println!("| {:>2} | {:<28} |", i, command);
+  let simple_queries = [
+    "GetDirectoriesPhotos",
+    "GetDirectoriesVideos",
+    "GetMediaCountPhotos",
+    "GetMediaCountVideos",
+  ];
+
+  let complex_queries = [
+    "GetPhoto(u8)",
+    "GetVideo(u8)",
+  ];
+
+  let all_printed = [
+    simple_commands.to_vec(),
+    complex_commands.to_vec(),
+    simple_queries.to_vec(),
+    complex_queries.to_vec(),
+  ];
+
+  for list in all_printed.iter() {
+    let bar = "+----+------------------------------+";
+    println!("{}", bar);
+    println!("| ID | Command Name                 |");
+    println!("{}", bar);
+  
+    for (i, item) in list.iter().enumerate() {
+      println!("| {:>2} | {:<28} |", i, item);
+    }
+  
+    println!("{}", bar);
   }
-
-  println!("{}", header);
 }
 
 #[tokio::main]
@@ -42,37 +71,37 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let command: &str = destructured_command[0];
 
     let simple_command_enum: Option<A8MiniSimpleCommand> = match command {
-      "0"   | "AutoCenter" => Some(A8MiniSimpleCommand::AutoCenter),
-      "1"   | "RotateUp" => Some(A8MiniSimpleCommand::RotateUp),
-      "2"   | "RotateDown" => Some(A8MiniSimpleCommand::RotateDown),
-      "3"   | "RotateRight" => Some(A8MiniSimpleCommand::RotateRight),
-      "4"   | "RotateLeft" => Some(A8MiniSimpleCommand::RotateLeft),
-      "5"   | "StopRotation" => Some(A8MiniSimpleCommand::StopRotation),
-      "6"   | "ZoomIn" => Some(A8MiniSimpleCommand::ZoomIn),
-      "7"   | "ZoomOut" => Some(A8MiniSimpleCommand::ZoomOut),
-      "8"   | "ZoomMax" => Some(A8MiniSimpleCommand::ZoomMax),
-      "9"   | "MaxZoomInformation" => Some(A8MiniSimpleCommand::MaxZoomInformation),
-      "10"  | "FocusIn" => Some(A8MiniSimpleCommand::FocusIn),
-      "11"  | "FocusOut" => Some(A8MiniSimpleCommand::FocusOut),
-      "12"  | "TakePicture" => Some(A8MiniSimpleCommand::TakePicture),
-      "13"  | "RecordVideo" => Some(A8MiniSimpleCommand::RecordVideo),
-      "14"  | "Rotate100100" => Some(A8MiniSimpleCommand::Rotate100100),
-      "15"  | "CameraInformation" => Some(A8MiniSimpleCommand::CameraInformation),
-      "16"  | "AutoFocus" => Some(A8MiniSimpleCommand::AutoFocus),
-      "17"  | "HardwareIDInformation" => Some(A8MiniSimpleCommand::HardwareIDInformation),
-      "18"  | "FirmwareVersionInformation" => Some(A8MiniSimpleCommand::FirmwareVersionInformation),
-      "19"  | "SetLockMode" => Some(A8MiniSimpleCommand::SetLockMode),
-      "20"  | "SetFollowMode" => Some(A8MiniSimpleCommand::SetFollowMode),
-      "21"  | "SetFPVMode" => Some(A8MiniSimpleCommand::SetFPVMode),
-      "22"  | "AttitudeInformation" => Some(A8MiniSimpleCommand::AttitudeInformation),
-      "23"  | "SetVideoOutputHDMI" => Some(A8MiniSimpleCommand::SetVideoOutputHDMI),
-      "24"  | "SetVideoOutputCVBS" => Some(A8MiniSimpleCommand::SetVideoOutputCVBS),
-      "25"  | "SetVideoOutputOff" => Some(A8MiniSimpleCommand::SetVideoOutputOff),
-      "26"  | "LaserRangefinderInformation" => Some(A8MiniSimpleCommand::LaserRangefinderInformation),
-      "27"  | "RebootCamera" => Some(A8MiniSimpleCommand::RebootCamera),
-      "28"  | "RebootGimbal" => Some(A8MiniSimpleCommand::RebootGimbal),
-      "29"  | "Resolution4k" => Some(A8MiniSimpleCommand::Resolution4k),
-      "30"  | "Heartbeat" => Some(A8MiniSimpleCommand::Heartbeat),
+      "AutoCenter" => Some(A8MiniSimpleCommand::AutoCenter),
+      "RotateUp" => Some(A8MiniSimpleCommand::RotateUp),
+      "RotateDown" => Some(A8MiniSimpleCommand::RotateDown),
+      "RotateRight" => Some(A8MiniSimpleCommand::RotateRight),
+      "RotateLeft" => Some(A8MiniSimpleCommand::RotateLeft),
+      "StopRotation" => Some(A8MiniSimpleCommand::StopRotation),
+      "ZoomIn" => Some(A8MiniSimpleCommand::ZoomIn),
+      "ZoomOut" => Some(A8MiniSimpleCommand::ZoomOut),
+      "ZoomMax" => Some(A8MiniSimpleCommand::ZoomMax),
+      "MaxZoomInformation" => Some(A8MiniSimpleCommand::MaxZoomInformation),
+      "FocusIn" => Some(A8MiniSimpleCommand::FocusIn),
+      "FocusOut" => Some(A8MiniSimpleCommand::FocusOut),
+      "TakePicture" => Some(A8MiniSimpleCommand::TakePicture),
+      "RecordVideo" => Some(A8MiniSimpleCommand::RecordVideo),
+      "Rotate100100" => Some(A8MiniSimpleCommand::Rotate100100),
+      "CameraInformation" => Some(A8MiniSimpleCommand::CameraInformation),
+      "AutoFocus" => Some(A8MiniSimpleCommand::AutoFocus),
+      "HardwareIDInformation" => Some(A8MiniSimpleCommand::HardwareIDInformation),
+      "FirmwareVersionInformation" => Some(A8MiniSimpleCommand::FirmwareVersionInformation),
+      "SetLockMode" => Some(A8MiniSimpleCommand::SetLockMode),
+      "SetFollowMode" => Some(A8MiniSimpleCommand::SetFollowMode),
+      "SetFPVMode" => Some(A8MiniSimpleCommand::SetFPVMode),
+      "AttitudeInformation" => Some(A8MiniSimpleCommand::AttitudeInformation),
+      "SetVideoOutputHDMI" => Some(A8MiniSimpleCommand::SetVideoOutputHDMI),
+      "SetVideoOutputCVBS" => Some(A8MiniSimpleCommand::SetVideoOutputCVBS),
+      "SetVideoOutputOff" => Some(A8MiniSimpleCommand::SetVideoOutputOff),
+      "LaserRangefinderInformation" => Some(A8MiniSimpleCommand::LaserRangefinderInformation),
+      "RebootCamera" => Some(A8MiniSimpleCommand::RebootCamera),
+      "RebootGimbal" => Some(A8MiniSimpleCommand::RebootGimbal),
+      "Resolution4k" => Some(A8MiniSimpleCommand::Resolution4k),
+      "Heartbeat" => Some(A8MiniSimpleCommand::Heartbeat),
       _ => None,
     };
 
@@ -83,17 +112,29 @@ async fn main() -> Result<(), Box<dyn Error>> {
       continue;
     }
 
-    let command_yaw: i16 = 0;
-    let command_pitch: i16 = 0;
-    
-    if destructured_command.len() == 3 {
-      destructured_command[1].parse().unwrap_or(0);
-      destructured_command[2].parse().unwrap_or(0);
-    }
-    
-    let complex_command_enum: Option<A8MiniComplexCommand> = match (command, command_yaw, command_pitch) {
-      ("29", yaw, pitch) | ("SetYawPitchSpeed", yaw, pitch) => Some(A8MiniComplexCommand::SetYawPitchSpeed(yaw as i8, pitch as i8)),
-      ("30", yaw, pitch) | ("SetYawPitchAngle", yaw, pitch) => Some(A8MiniComplexCommand::SetYawPitchAngle(yaw, pitch)),
+    let complex_command_enum: Option<A8MiniComplexCommand> = match command {
+      "SetYawPitchSpeed" => {
+        let yaw: i8 = destructured_command[1].parse().unwrap_or(0);
+        let pitch: i8 = destructured_command[2].parse().unwrap_or(0);
+        Some(A8MiniComplexCommand::SetYawPitchSpeed(yaw, pitch))
+      },
+      "SetYawPitchAngle" => {
+        let yaw: i16 = destructured_command[1].parse().unwrap_or(0);
+        let pitch: i16 = destructured_command[2].parse().unwrap_or(0);
+        Some(A8MiniComplexCommand::SetYawPitchAngle(yaw, pitch))
+      },
+      "SetTimeUTC" => {
+        let epoch: u64 = destructured_command[1].parse().unwrap_or(0);
+        Some(A8MiniComplexCommand::SetTimeUTC(epoch))
+      },
+      "GetCodecSpecs" => {
+        let stream_type: u8 = destructured_command[1].parse().unwrap_or(0);
+        Some(A8MiniComplexCommand::GetCodecSpecs(stream_type))
+      },
+      "SetCodecSpecs" => {
+        let stream_type: u8 = destructured_command[1].parse().unwrap_or(0);
+        Some(A8MiniComplexCommand::SetCodecSpecs(stream_type, 2, 3840, 2160, 50000, 0))
+      },
       _ => None,
     };
 
@@ -104,12 +145,40 @@ async fn main() -> Result<(), Box<dyn Error>> {
       continue;
     }
 
-    // TODO: restructure this loop in a smarter way instead of spamming goto's
-    else {
-      println!("Unrecognized Command: {:?}", destructured_command);
-      break;
+    let simple_query_enum: Option<A8MiniSimpleHTTPQuery> = match command {
+      "GetDirectoriesPhotos" => Some(A8MiniSimpleHTTPQuery::GetDirectoriesPhotos),
+      "GetDirectoriesVideos" => Some(A8MiniSimpleHTTPQuery::GetDirectoriesVideos),
+      "GetMediaCountPhotos"  => Some(A8MiniSimpleHTTPQuery::GetMediaCountPhotos),
+      "GetMediaCountVideos"  => Some(A8MiniSimpleHTTPQuery::GetMediaCountVideos),
+      _ => None,
+    };
+
+    if simple_query_enum.is_some() {
+      println!("Sending Simple HTTP Query {:?}", simple_query_enum.unwrap());
+      let camera: A8Mini = A8Mini::connect().await?;
+      let response = camera.send_http_query(simple_query_enum.unwrap()).await?;
+      println!("{:?}", response);
+      continue;
+    }
+
+    let complex_query_enum: Option<A8MiniComplexHTTPQuery> = match command {
+      "GetPhoto" => {
+        let photo_ind: u8 = destructured_command[1].parse().unwrap_or(0);
+        Some(A8MiniComplexHTTPQuery::GetPhoto(photo_ind))
+      }
+      "GetVideo" => {
+        let video_ind: u8 = destructured_command[1].parse().unwrap_or(0);
+        Some(A8MiniComplexHTTPQuery::GetVideo(video_ind))
+      }
+      _ => None,
+    };
+
+    if complex_query_enum.is_some() {
+      println!("Sending Complex HTTP Query {:?}", complex_query_enum.unwrap());
+      let camera: A8Mini = A8Mini::connect().await?;
+      let response = camera.send_http_query(complex_query_enum.unwrap()).await?;
+      println!("{:?}", response);
+      continue;
     }
   }
-
-  Ok(())
 }
